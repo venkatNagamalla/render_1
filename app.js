ff668409bdeb9427e6052f9f200b5551d442ff80
venkat <shivavenkat006@gmail.com>
2024-04-30 19:22:38 +0530

    4

--- app.js
+++ app.js
@@ -1,41 +1,11 @@
 
 const express = require('express')
 const app = express()
-app.use(express.json())
-const {open} = require('sqlite')
-const sqlite3 = require('sqlite3')
-const path = require('path')
-const dbPath = path.join(__dirname,'users.db')
-let db = null
 
-const initializeDbAndServer = async() => {
-    try{
-        db = await open({
-            filename: dbPath,
-            driver: sqlite3.Database
-        })
-        const port = process.env.PORT || 3000
-        app.listen(port, ()=> {
-            console.log('app is listening at localhost:3000')
-        })
-    }catch(e){
-        console.log(e.message)
-        process.exit(1)
-    }
-}
-
-
-app.get("/users/",async (request,response)=> {
-    const getAllUsers = `SELECT * FROM user`;
-    const allUsers = await db.all(getAllUsers)
-    response.send(allUsers)
+app.listen(process.env.PORT || 3000, () =>{
+    console.log('LlL')
 })
 
-app.get('/users/:userId',async(request,response) => {
-    const {userId}  = request.params 
-    const getUser = `SELECT * FROM user WHERE id=${userId}`;
-    const user = await db.get(getUser)
-    response.send(user)
+app.get("/", (request,response)=> {
+    response.send("HJJJ")
 })
-
-initializeDbAndServer()
